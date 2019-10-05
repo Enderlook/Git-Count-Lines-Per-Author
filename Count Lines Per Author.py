3f4420f7a740c353d70588d3c7eab7cf1b524b2d
Enderlook <enderlook@gmail.com>
2019-10-04 21:37:07 -0300

    Add change counter per file extension

--- Count Lines Per Author.py
+++ Count Lines Per Author.py
@@ -1,14 +1,12 @@
 import subprocess, re
+from collections import defaultdict
 
 insertions = re.compile("(\d+) insertions?\(\+\)")
 deletions = re.compile("(\d+) deletions?\(\-\)")
 
 main_process = subprocess.Popen("git shortlog -s -n --all", stdout=subprocess.PIPE)
 
-for line_user in main_process.stdout:
-    line = line_user.rstrip()
-    user = line.split(b"\t")[1].decode("utf-8")
-    
+def basic_process(user):
     user_process = subprocess.Popen(f'git log --author="{user}" --pretty=tformat: --shortstat', stdout=subprocess.PIPE, shell = False)
     total_insertions = 0
     total_deletions = 0
@@ -22,3 +20,26 @@ for line_user in main_process.stdout:
             total_deletions += int(delete.group(1))
     result = total_insertions - total_deletions
     print(f"{user}: + {total_insertions} - {total_deletions} = {result}")
+
+def advanced_process(user):
+    user_process = subprocess.Popen(f'git log --author="{user}" --pretty=tformat:"%H" --numstat --oneline', stdout=subprocess.PIPE, shell = False)
+    files = defaultdict(lambda: defaultdict(int))
+    for line_user in user_process.stdout:
+        line = line_user.decode()
+        attributes = line.split("\t");
+        if len(attributes) == 3 and (attributes[0] is not "-" or attributes[1] is not "-"):
+            extension = attributes[2].split(".")[-1].strip()
+            files[extension]["+"] += int(attributes[0])
+            files[extension]["-"] += int(attributes[1])
+    print(user + ":")
+    for k, v in files.items():
+        print(f"\t.{k}: + {v['+']} - {v['-']} = {v['+'] - v['-']}")
+
+for line_user in main_process.stdout:
+    line = line_user.rstrip()
+    user = line.split(b"\t")[1].decode("utf-8")
+
+    advanced_process(user)
+
+
+
