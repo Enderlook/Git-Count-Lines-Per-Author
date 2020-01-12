2ed23f879dc26d0d90ec50753900f3d01e09f574
Enderlook <enderlook@gmail.com>
2020-01-12 19:59:36 -0300

    Add output to output.txt

--- Count Lines Per Author.py
+++ Count Lines Per Author.py
@@ -15,7 +15,7 @@ def basic_process(name, email):
         if delete:
             total_deletions += int(delete.group(1))
     result = total_insertions - total_deletions
-    print(f"{name} {email}: + {total_insertions} - {total_deletions} = {result}")
+    return f"{name} {email}: + {total_insertions} - {total_deletions} = {result}"
 
 def advanced_process(name, email):
     user_process = Popen(f'git log --author="{name}" --pretty=tformat:"%H" --numstat --oneline', stdout=PIPE, shell = False)
@@ -25,16 +25,19 @@ def advanced_process(name, email):
         line = line_user.decode()
         attributes = line.split("\t");
         if len(attributes) == 3 and (attributes[0] is not "-" or attributes[1] is not "-"):
-            extension = attributes[2].split(".")[-1].strip()
+            extension = attributes[2].split(".")[-1].split("/")[-1]
+            extension = extension.split("=>")[-1].replace("}", "").strip()
             insertions = int(attributes[0])
             files[extension]["+"] += insertions
             total_insertions += insertions
             deletions = int(attributes[1])
             files[extension]["-"] += deletions
             total_deletions += int(attributes[1])
-    print(f"{name} {email}: + {total_insertions} - {total_deletions} = {total_insertions - total_deletions}")    
+    output = []
+    output.append(f"{name} {email}: + {total_insertions} - {total_deletions} = {total_insertions - total_deletions}")
     for k, v in files.items():
-        print(f"\t.{k}: + {v['+']} - {v['-']} = {v['+'] - v['-']}")
+        output.append(f"\t.{k}: + {v['+']} - {v['-']} = {v['+'] - v['-']}")
+    return output
 
 def get_authors():
     main_process = Popen("git shortlog -s -n --all -e", stdout=PIPE)
@@ -46,8 +49,12 @@ def main():
     insertions = compile("(\d+) insertions?\(\+\)")
     deletions = compile("(\d+) deletions?\(\-\)")
 
-    with Pool() as pool:
-        pool.starmap(advanced_process, get_authors())
+    with open("output.txt", "w", encoding="utf-8") as file:
+        with Pool() as pool:
+            for process in pool.starmap(advanced_process, get_authors()):
+                for line in process:
+                    print(line)
+                    file.write(line + "\n")
 
 if __name__ == "__main__":
     main()
